7cf7f9f35c91f43015f2f4915e0b5d2e7116cd79
icetrextrades-pixel <icetrextrades@gmail.com>
2026-04-20 14:15:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -190,3 +190,4 @@ def show_dashboard():
             <div style="border: 2px solid {c}; padding: 20px; border-radius: 15px; background: rgba(0,0,0,0.7);">
                 <h1 style="color:{c}; font-size:90px; margin:0;">{v}x</h1>
                 <p style="color:{c}; font-weight:bold;">ACCURACY: {random.randint(78, 8)}
+    """, unsafe_allow_html=True)
