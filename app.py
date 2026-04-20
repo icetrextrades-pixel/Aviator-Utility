10f1bf1e1c257f157930d4bf7e6811b306b29fc6
icetrextrades-pixel <icetrextrades@gmail.com>
2026-04-20 22:55:48 +0200

    Refactor app.py for improved structure and styling

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def show_login():
     k = st.text_input("KEY", type="password")
     if st.button("ACTIVATE"):
         if u == "Icetrex" and k == "SOPITO":
-            u == "AUSTIN" and k == "tinofa2578":
+            u == "AUSTIN" and k == "tinofa2578",
             st.session_state["pass"] = True
             st.rerun()
         else:
